4a51061220b9f2d425ff4096679f2a3111112cc0
John Norman <john@suckerfreegames.com>
2020-10-18 17:55:55 -0700

    Re-wrote resource handling.

--- src/engine/resources/Resource.h
+++ src/engine/resources/Resource.h
@@ -15,18 +15,23 @@ class Resource
 {
 public:
 	Resource() = delete;
-	Resource(const Resource& other) = delete;
-	Resource(const Resource&& other) = delete;
+	Resource(const Resource& other)
+		: m_name(other.m_name), m_underlying(other.m_underlying)
+	{}
 
-	Resource(const std::string& name, std::unique_ptr<T> underlying)
-		: m_name(name), m_underlying(std::move(underlying))
-	{};
+	Resource(const Resource&& other)
+		: m_name(std::move(other.m_name)), m_underlying(std::move(other.m_underlying))
+	{}
 
 	Resource(const std::string& name, T* underlying)
 		: m_name(name), m_underlying(underlying)
-	{};
+	{}
 
-	virtual ~Resource() {};
+	Resource(const std::string& name, std::shared_ptr<T>&& underlying)
+		: m_name(name), m_underlying(std::move(underlying))
+	{}
+
+	virtual ~Resource() {}
 
 	Resource& operator=(const Resource& other) = delete;
 	Resource& operator=(Resource&& other) = delete;
@@ -43,100 +48,19 @@ public:
 		return static_cast<T>(m_underlying.get());
 	}
 
-	T* Get() const
+	std::weak_ptr<T> Get() const
 	{
-		return m_underlying.get();
+		return std::weak_ptr<T>(m_underlying);
 	}
 
 	const std::string& GetName() const
 	{
 		return m_name;
-	};
+	}
 
 protected:
-	std::unique_ptr<T> m_underlying;
+	std::shared_ptr<T> m_underlying;
 	const std::string m_name;
-};
-
-
-/**
- * Handle to a resource
- */
-template <typename T>
-class ResourceHandle
-{
-public:
-	ResourceHandle() {};
-
-	ResourceHandle(const std::shared_ptr<const Resource<T>>& resource)
-		: m_resource(resource)
-	{};
-
-	ResourceHandle(const ResourceHandle<T>& other)
-		: ResourceHandle(other.GetResource())
-	{};
-
-	ResourceHandle(ResourceHandle<T>&& other) noexcept
-		: m_resource(other.GetResource())
-	{
-		other.m_resource.reset();
-	};
-
-	~ResourceHandle() noexcept {
-	};
-
-public:
-	ResourceHandle<T>& operator=(const ResourceHandle<T>& other)
-	{
-		ResourceHandle<T> tmp(other);
-		*this = std::move(tmp);
-
-		return *this;
-	};
-
-	ResourceHandle<T>& operator=(const ResourceHandle<T>&& other)
-	{
-		if (this == &other)
-			return *this;
-
-		m_resource = other.m_resource;
-		other.m_resource.reset();
-
-		return *this;
-	};
-
-	bool operator==(const ResourceHandle& other) const
-	{
-		return m_resource == other.m_resource;
-	};
-
-	operator T* const()
-	{
-		return static_cast<T>(m_resource.Get());
-	}
-
-	operator const T* const () const
-	{
-		return static_cast<T>(m_resouce.Get());
-	}
-
-	T* operator->() const
-	{
-		return m_resource->Get();
-	};
-
-	bool Empty() const
-	{
-		return m_resource == nullptr;
-	}
-
-	const std::string& GetName() const
-	{
-		return m_resource->GetName();
-	};
-
-private:
-	const std::shared_ptr<const Resource<T>> m_resource;
 };
 
 
@@ -158,26 +82,34 @@ public:
 
 public:
 	template <typename T>
-	ResourceHandle<T> Get(size_t id)
+	std::weak_ptr<T> Get(size_t id)
 	{
-		auto r = m_resources[typeid(T).hash_code()];
+		auto hash = typeid(T).hash_code();
+		auto& r = m_resources[hash];
+
 		auto i = r.find(id);
 		if (i == r.end())
 			return {};
 
-		auto p = std::shared_ptr<Resource<T>>{ std::any_cast<Resource<T>>(i->second) };
-		return ResourceHandle<T>(p);
+		Resource<T>* p = std::any_cast<Resource<T>>(&i->second);
+		if (p == nullptr) return {};
+
+		return p->Get();
 	}
 
 	template <typename T>
 	size_t FindId(const std::string& name)
 	{
-		auto r = m_resources[typeid(T).hash_code()];
+		auto hash = typeid(T).hash_code();
+		auto& r = m_resources[hash];
 
 		for (auto&& [key, value] : r)
 		{
-			if (std::any_cast<Resource<T>>(value).GetName() == name)
-				return key;
+			if (Resource<T>* p = std::any_cast<Resource<T>>(&value))
+			{
+				if (p->GetName() == name)
+					return key;
+			}
 		}
 
 		return 0;
@@ -186,18 +118,21 @@ public:
 	template <typename T>
 	size_t Add(const std::string& name, T* resource)
 	{
-		return Add<T>(name, std::unique_ptr<T>(resource));
+		return Add<T>(name, std::move(std::shared_ptr<T>(resource)));
 	}
 
 	template <typename T>
-	size_t Add(const std::string& name, std::unique_ptr<T> resource)
+	size_t Add(const std::string& name, std::shared_ptr<T>&& resource)
 	{
-		auto r = m_resources[typeid(T).hash_code()];
+		auto hash = typeid(T).hash_code();
+		auto& r = m_resources[hash];
 
 		auto id = std::hash<std::string>()(name);
-		std::any a = Resource<T>(name, resource);
+		auto a = std::make_any<Resource<T>>(name, std::move(resource));
 
-		r.insert(std::make_pair(id, a));
+		auto entry = std::make_pair(id, a);
+		r.insert(entry);
+
 		return id;
 	}
 
